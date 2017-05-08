4acd744ac40b46ce9e9697f224d8f9b1884d4419
Jonathon Israel <jisrael11@live.com>
2017-05-07 21:49:34 -0500

    Initial upload
    initial upload

---
+++ Locked in Laferre/Assets/Scripts/PauseMenu/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject playerCamera;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            this.gameObject.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            playerCamera.GetComponent<CamMouseLook>().enabled = true;
+            player.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+}
---
+++ Locked in Laferre/Assets/Scripts/PlayerRaycast.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour {
+
+    public float distanceToSee;
+    RaycastHit whatIHit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
+
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee) && Input.GetButton("Fire1"))
+        {
+            Debug.Log("I touched something " + whatIHit.collider.gameObject.name);
+            //if(whatIHit.collider.gameObject.GetComponent</*name of script*/>().)
+        }
+	}
+}
