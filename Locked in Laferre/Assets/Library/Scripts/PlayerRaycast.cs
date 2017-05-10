95435202e6979e5e865a9486fe5fa052613c98de
Jonathon Israel <jisrael11@live.com>
2017-05-09 23:59:47 -0500

    completed Project
    Complet Project

---
+++ Locked in Laferre/Assets/Library/Scripts/PlayerRaycast.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerRaycast : MonoBehaviour {
+
+    public float distanceToSee;
+
+    private bool key = false;
+
+    RaycastHit clue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
+
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out clue, distanceToSee) && Input.GetButtonDown("Fire1"))
+        {
+            if(clue.collider.tag == "Clues")
+            {
+                clue.collider.gameObject.SendMessageUpwards("collision");
+            } else if (clue.collider.tag == "Key")
+            {
+                key = true;
+                Destroy(clue.collider.gameObject);
+            } else if (clue.collider.tag == "Lock" && key)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                SceneManager.LoadScene(2);
+            }
+        }
+	}
+}
