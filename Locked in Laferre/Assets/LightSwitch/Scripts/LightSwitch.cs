39de1ab1c213965440b8b0ca2f196d28ef96760b
Jonathon Israel <jisrael11@live.com>
2017-05-09 19:06:16 -0500

    changes
    changes

---
+++ Locked in Laferre/Assets/LightSwitch/Scripts/LightSwitch.cs
@@ -0,0 +1,72 @@
+﻿//LightSwitch Script
+//IMPORTANT: PLEASE READ
+//......................
+// IN ORDER FOR THIS SCRIPT
+// TO RUN, YOU MUST ADD A
+// BOX COLLIDER TO YOUR 
+//LIGHTSWITCH!
+//.......................
+//THIS SCRIPT IS TRIGGERED
+//BY CLICKING THE LIGHTSWITCH
+//BOX COLLIDER
+//......................
+
+
+using UnityEngine;
+using System.Collections;
+
+	// If you change the name of your script, change the public
+	// class below from LightSwitch to what you named the script.
+public class LightSwitch : MonoBehaviour {
+
+	// Declares the light game object as "DragYourLightHere".
+	// Do not try to drag your light from unity into the actual script.
+	// It is named like this so that the Unity editor will display where to
+	// drag your light.
+	public GameObject DragYourLightHere;
+
+	// Declares the two states for the lightswitch: on or off.
+	private enum State {
+		on,
+		off
+	}
+
+	// Declares the State as state.
+	private State state;
+
+	// Initializes the lightswitch so that the script knows the lights are off to
+	// start with.
+	void Start () {
+		state = LightSwitch.State.off;
+
+	}
+
+	// On the mouse up event, the state changes to off if it was on before clicked, and
+	// the state changes to on if it was off before clicked.
+	public void collision() {
+        Debug.Log("I flipped the switch.");
+		if (state == LightSwitch.State.on)
+						TurnOff ();
+				else
+						TurnOn ();
+	}
+
+	// Turn on subprocedure: plays the TurnOn Animation, sets the state to on, and enables
+	// the light.
+	private void TurnOn() {
+		GetComponent<Animation>().Play ("TurnOnAnimation");
+		state = LightSwitch.State.on;
+        DragYourLightHere.SetActive(true);
+
+}
+
+	//Turn off subprocedure: plays the TurnOff Animation, sets the state to off, and
+	// disables the light.
+	private void TurnOff() {
+		GetComponent<Animation>().Play ("TurnOffAnimation");
+		state = LightSwitch.State.off;
+        DragYourLightHere.SetActive(false);
+
+}
+}
+
---
+++ Locked in Laferre/Assets/Scripts/MoveFiling.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFiling : MonoBehaviour {
+
+    Vector3 movement;
+
+    // Use this for initialization
+    void Start()
+    {
+        movement = new Vector3(transform.position.x, 2.496f , transform.position.z);
+    }
+
+    public void collision()
+    {
+        transform.position = movement;
+    }
+}
